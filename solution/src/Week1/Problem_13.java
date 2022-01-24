2af82d83389c90eef21bb670bf58e0d3960b973c
Key-Study <kiryungnam@doai.ai>
2022-01-24 10:35:23 +0900

    feature : Problem_14

--- solution/src/Week1/Problem_13.java
+++ solution/src/Week1/Problem_13.java
@@ -2,8 +2,7 @@ package Week1;
 
 // Substraction이 아니라면 모두 각 캐릭터가 의미하는 숫자를 더한것
 
-class Problem_3 {
-
+class Problem_13 {
     static final char ONE = 'I';
     static final char FIVE = 'V';
     static final char TEN = 'X';
@@ -12,31 +11,45 @@ class Problem_3 {
     static final char FIVE_HUNDREDS = 'D';
     static final char THOUSAND = 'M';
 
+    public static void main(String[] args) {
+
+    }
+
     public int romanToInt(String s) {
         int number = 0;
         int index = 0;
-        int substractionFlag = false;
 
         while (index < s.length()) {
-            char numberChar = s.charAt(index);
-
-
-
+            int subResult = getNumericUsingPrinciple(s, index);
+            if (subResult == -1) {
+                number += getNumericFromRoman(s.charAt(index));
+                index++;
+            } else {
+                number += subResult;
+                index = index + 2;
+            }
         }
+
+        return number;
     }
 
-    private int getNumericFromSub(String romanString, int currentIndex) {
-        if (romanString.length() == currentIndex) {
+    private int getNumericUsingPrinciple(String romanString, int currentIndex) {
+        int nextIndex = currentIndex + 1;
 
+        if (romanString.length() <= nextIndex) {
+            return -1;
         }
 
         char currentChar = romanString.charAt(currentIndex);
+        char nextChar = romanString.charAt(nextIndex);
 
         if (currentChar == ONE) {
             if (nextChar == FIVE) {
                 return 4;
             } else if (nextChar == TEN) {
                 return 9;
+            } else {
+                return -1;
             }
         }
 
@@ -45,6 +58,8 @@ class Problem_3 {
                 return 40;
             } else if (nextChar == HUNDRED) {
                 return 90;
+            } else {
+                return -1;
             }
         }
 
@@ -53,7 +68,24 @@ class Problem_3 {
                 return 400;
             } else if (nextChar == THOUSAND) {
                 return 900;
+            } else {
+                return -1;
             }
         }
+
+        return -1;
+    }
+
+    private int getNumericFromRoman(char romanCharacter) {
+        return switch (romanCharacter) {
+            case ONE -> 1;
+            case FIVE -> 5;
+            case TEN -> 10;
+            case FIFTY -> 50;
+            case HUNDRED -> 100;
+            case FIVE_HUNDREDS -> 500;
+            case THOUSAND -> 1000;
+            default -> 0;
+        };
     }
 }
---
+++ solution/src/Week1/Problem_14.java
@@ -0,0 +1,35 @@
+package Week1;
+
+class Problem_14 {
+    public String longestCommonPrefix(String[] strs) {
+
+        StringBuilder sb = new StringBuilder();
+        char prefixParticipant = Character.MIN_VALUE;
+        int prefixIndex = 0;
+        boolean exitFlag = false;
+
+        arrayexit:
+        while(!exitFlag) {
+            for (String str : strs) {
+                if (prefixIndex >= str.length()) {
+                    return sb.toString();
+                }
+
+                if (prefixParticipant == Character.MIN_VALUE) {
+                    prefixParticipant = str.charAt(prefixIndex);
+                }
+
+                if (prefixParticipant != str.charAt(prefixIndex)) {
+                    exitFlag = true;
+                    break arrayexit;
+                }
+            }
+
+            sb.append(prefixParticipant);
+            prefixIndex++;
+            prefixParticipant = Character.MIN_VALUE;
+        }
+
+        return sb.toString();
+    }
+}
