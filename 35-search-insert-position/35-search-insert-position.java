6e6731da99181cb6387a63da8be34fcd92b38432
Key-Study <kakapo12@naver.com>
2022-03-19 09:46:47 +0900

    Time: 0 ms (100.00%), Space: 43.4 MB (42.90%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        return searchInsertRecursive(nums, 0, nums.length - 1, target);
+    }
+
+    private int searchInsertRecursive(int[] nums, int start, int end, int target) {
+        if (start >= end) {
+            if (nums[start] >= target) {
+                return start;
+            } else {
+                return start + 1;
+            }
+        }
+
+        int mid = (start + end) / 2;
+        int lindex = -1;
+        int rindex = -1;
+        if (target <= nums[mid]) {
+            lindex = searchInsertRecursive(nums, start, mid, target);
+        } else {
+            rindex = searchInsertRecursive(nums, mid + 1, end, target);
+        }
+
+        return Math.max(rindex, lindex);
+    }
+}
