31b133859c569d14cf7c9f9e479bd702bfce80ff
Be-The-Key <kakapo12@naver.com>
2022-04-21 09:14:57 +0900

    Time: 0 ms (100.00%), Space: 38.9 MB (92.34%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.java
@@ -9,7 +9,7 @@
 
 public class Solution extends GuessGame {
     public int guessNumber(int n) {
-        return guessRecursive(1, n);
+        return guessLoop(n);
     }
     
     private int guessRecursive(int start, int end) {
@@ -27,8 +27,9 @@ public class Solution extends GuessGame {
     private int guessLoop(int n) {
         int start = 1;
         int end = n;
-        while (start >= end) {
-            int mid = start + (end - start) >> 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
             int result = guess(mid);    
             if (result == 0) {
                 return mid;
