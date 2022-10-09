c8bfa8adcbc3046f79019a92da858bf2a2a4012f
Be-The-Key <rlfbd5142@gmail.com>
2022-10-10 00:07:30 +0900

    Time: 3 ms (93.22%), Space: 42.5 MB (89.50%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,31 +1,17 @@
-// - 하나의 답은 항상 갖고 있음
-// - 중복은 허용안함
-
-// 1. 기준점을 바깥 loop에서 선택
-// 2. 더해지는 숫자를 다음 index부터 찾아나가면서 target과 같은지 확인
-
-// -> 순열
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hashMaps = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashMaps.put(nums[i], i);
+        }
         
-        int lhs;
-        int rhs;
-        int[] resultIndex = new int[]{-1, -1};
-        for (int i = 0; i < nums.length - 1; i++) {
-            lhs = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                rhs = nums[j];
-                
-                if ((lhs + rhs) == target) {
-                    resultIndex[0] = i;
-                    resultIndex[1] = j;
-                    return resultIndex;
-                } else {
-                    continue;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (hashMaps.containsKey(complement) && hashMaps.get(complement) != i) {
+                return new int[] {i, hashMaps.get(complement)};
             }
         }
         
-        return resultIndex;
+        return new int[] {-1, -1};
     }
 }
