47ae4748ff9a10124927f61fb7a05620511f7444
Be-The-Key <rlfbd5142@gmail.com>
2022-10-09 23:53:22 +0900

    Time: 3 ms (27.58%), Space: 74.2 MB (38.74%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,29 +1,14 @@
 class Solution {
-    
-//     public int maxSubArray(int[] nums) {
-//         int maxSubArray = Integer.MIN_VALUE;
-        
-//         for (int i = 0; i < nums.length; i++) {
-//             int currentSubArray = 0;
-//             for (int j = i; j < nums.length; j++) {
-//                 currentSubArray += nums[j];
-//                 maxSubArray = Math.max(maxSubArray, currentSubArray);
-//             }
-//         }
-        
-//         return maxSubArray;
-//     }
-    
-    //DP 솔루션 다시 생각
     public int maxSubArray(int[] nums) {
-        int allMax = nums[0];
-        int curMax = nums[0];
-
-        for (int i = 1; i < nums.length; i++) {
-            curMax = Math.max(curMax + nums[i], nums[i]);
-            allMax = Math.max(allMax, curMax);
+        int[] dp = new int[nums.length];
+        int maxSum = nums[0];
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            maxSum = Math.max(maxSum, dp[i]);
         }
-
-        return allMax;
+        
+        return maxSum;
     }
 }
