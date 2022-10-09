4ba3cc7f39b8510a821ca0ae88b3642c4d438716
Be-The-Key <rlfbd5142@gmail.com>
2022-10-10 00:39:34 +0900

    Time: 0 ms (100.00%), Space: 41.1 MB (24.04%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,28 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return climbStairsRecursive(0, n, memo);
-    }
-
-    /**
-     * @param step
-     * @param result
-     * @return count of occurence
-     */
-    private int climbStairsRecursive(int step, int result, int memo[]) {
-        if (step > result) {
-            return 0;
-        }
-
-        if (step == result) {
+        if (n == 1) {
             return 1;
         }
-
-        if (memo[step] > 0) {
-            return memo[step];
+        int[] dpSteps = new int[n + 1];
+        dpSteps[1] = 1;
+        dpSteps[2] = 2;
+        
+        for (int i = 3; i < dpSteps.length; i++) {
+            dpSteps[i] = dpSteps[i - 1] + dpSteps[i - 2];
         }
-
-        memo[step] = climbStairsRecursive(step + 1, result, memo) + climbStairsRecursive(step + 2, result, memo);
-        return memo[step];
+        
+        return dpSteps[n];
     }
 }
