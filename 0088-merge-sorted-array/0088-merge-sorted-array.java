359229ad37c0897667e9aa00eee6d55537844c87
Be-The-Key <rlfbd5142@gmail.com>
2022-10-25 02:08:25 +0900

    Time: 1 ms (53.33%), Space: 43.3 MB (13.35%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,23 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int tail1 = m - 1;
-        int tail2 = n - 1;
-        int sortPoint = m + n - 1;
-
-        while (tail1 >= 0 && tail2 >= 0) {
-            if (nums1[tail1] >= nums2[tail2]) {
-                nums1[sortPoint--] = nums1[tail1--];
+        int p1 = m - 1;
+        int p2 = n - 1;
+        
+        int p = nums1.length - 1;
+        
+        while (p > -1) {
+            int cand1 = p1 > -1 ? nums1[p1] : Integer.MIN_VALUE;
+            int cand2 = p2 > -1 ? nums2[p2] : Integer.MIN_VALUE;
+            
+            if (cand1 > cand2) {
+                nums1[p] = cand1;
+                p1--;
             } else {
-                nums1[sortPoint--] = nums2[tail2--];
+                nums1[p] = cand2;
+                p2--;
             }
-        }
-
-        while (tail2 >= 0) {
-            nums1[sortPoint--] = nums2[tail2--];
+            
+            p--;
         }
     }
 }
